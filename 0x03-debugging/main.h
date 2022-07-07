5bd7e34ecee6fe3338da45be215d02a50f1efbf1
Daniel Mwai Tsuma <dmwaitsuma@gmail.com>
2022-07-07 16:25:25 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,39 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+/**
+ *  * largest_number - returns the largest of 3 numbers
+ *   * @a: first integer
+ *    * @b: second integer
+ *     * @c: third integer
+ *      * Return: largest number
+ *       */
+
+int largest_number(int a, int b, int c);
+
+/**
+ * * convert_day - converts day of month to day of year, without accounting
+ * * for leap year
+ * * @month: month in number format
+ * * @day: day of month
+ * * Return: day of year
+ * */
+
+int convert_day(int month, int day);
+
+/**
+ * * print_remaining_days - takes a date and prints how many days are
+ * * left in the year, taking leap years into account
+ * * @month: month in number format
+ * * @day: day of month
+ * * @year: year
+ * * Return: void
+ * */
+
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
